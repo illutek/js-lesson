07a22b8a98ba614318b1b32a7336ad884aa97393
Vandenborne Stefan <stefan@illutek.be>
2017-07-18 11:11:28 +0200

    first commit

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Stefan on 16/07/2017.
+ */
+
+// Comment
+    /*
+var name = 'John';
+
+console.log(name);
+
+var lastName = 'Smith';
+
+console.log(lastName);
+
+var age = 26;
+
+console.log(age);
+
+var ageFull = true
+
+console.log(name + age);
+
+console.log(age + age);
+
+var job, isMarried;
+
+
+isMarried = false;
+job = 'teacher';
+
+console.log(name + ' ' + age + ' ' + job);
+
+var lastName = prompt('What is the last name');
+
+console.log('your last name = ' + lastName);
+
+alert(name + ' ' + age + ' ' + job);
+        */
+
+var birthYear = 2016 - 26;
+
